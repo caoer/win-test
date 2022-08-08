8732914d08e825847976b8ed40766ebad7eddbad
Zitao Xiong <caoer115@gmail.com>
2022-08-09 00:38:44 +0800

    Revert "[WIP] - 🚧"
    This reverts commit 8c61aceb6faf11b9571b6ea92e1f00685e6f8306.

---
+++ Windows/user-create.bat
@@ -0,0 +1,15 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+net user Administrator Pass@!1270 /add >nul
+net localgroup administrators Administrator /add >nul
+net user Administrator /active:yes >nul
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant Administrator:F >nul
+ICACLS C:\Windows\installer /grant Administrator:F >nul
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+ping -n 10 127.0.0.1 >nul
